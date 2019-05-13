061e5409c1fe732ae04b44d84ed58387e710691e
krilek <krilek@gmail.com>
2019-05-14 00:45:22 +0200

    Added entity manager. It works.

--- Hipstagram/Startup.cs
+++ Hipstagram/Startup.cs
@@ -1,8 +1,10 @@
+using HipstagramService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -20,8 +22,10 @@ namespace Hipstagram
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            var connection = "Data Source=hipstagram.db";
+            services.AddDbContext<HipstagramContext>
+                (options => options.UseSqlite(connection));
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
@@ -39,6 +43,7 @@ namespace Hipstagram
             else
             {
                 app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
---
+++ HipstagramService/HipstagramContext.cs
@@ -0,0 +1,15 @@
+﻿using HipstagramService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HipstagramService
+{
+    public class HipstagramContext : DbContext
+    {
+        public HipstagramContext(DbContextOptions<HipstagramContext> options)
+            : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+
+}
