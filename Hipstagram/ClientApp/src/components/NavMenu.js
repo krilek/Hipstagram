100a8d755124daa8e1cd2ed1b68c18f4c580aaa4
Gzik, Karol <karol.gzik@intel.com>
2019-05-27 15:38:05 +0200

    Semi works I will end it with Szymon.

--- Hipstagram/ClientApp/src/components/NavMenu.js
+++ Hipstagram/ClientApp/src/components/NavMenu.js
@@ -63,7 +63,23 @@ export class NavMenu extends Component {
     });
   }
 
-  render () {
+    render() {
+        const navigation = this.navItems.map(navItem => {
+            if (this.state.signedIn && navItem.private || this.state.signedIn && !navItem.hideWhenLogged) {
+                return ( < NavItem >  < NavLink
+                tag = { Link }
+                className = "text-dark"
+                to = { navItem.to } > { navItem.content } < /
+                NavLink >  < /
+                NavItem > )
+
+            } else {
+                return
+            }
+
+
+        })
+
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
@@ -72,27 +88,7 @@ export class NavMenu extends Component {
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/login">Login</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/register">Register</NavLink>
-                </NavItem>
-                {this.state.signedIn &&
-                <NavItem>
-                    <NavLink tag={Link} className="text-dark" to="/gallery">Gallery</NavLink>
-                </NavItem>
-                }
-
+                            {navigation}
               </ul>
             </Collapse>
           </Container>
